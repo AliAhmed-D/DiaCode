5843e8c425c54353b0390429e42c5c67902327db
Ali Ahmed Abdul Hussein Zarki <2501230089@iku.edu.iq>
2026-04-26 00:07:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-self.addEventListener('fetch', function(event) {
-  
+const cacheName = 'diacode-v2';
+const assets = ['./', './index.html', './style.css', './script.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
