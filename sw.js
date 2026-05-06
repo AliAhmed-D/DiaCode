972bbcd4f824c18bc96db43e37ae00d6be58309e
Ali Ahmed Abdul Hussein Zarki <2501230089@iku.edu.iq>
2026-05-06 21:43:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const cacheName = 'diacode-v2';
-const assets = ['./', './index.html', './style.css', './script.js'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
